29b58ff4dd47998bc73633ed98ea532cf93b04e7
Xwei <tsai.dennis12@gmail.com>
2017-08-17 22:36:52 +0800

    Init Data/GoogleNews-w2v.sh

---
+++ Data/GoogleNews-w2v.sh
@@ -0,0 +1,5 @@
+# download  GoogleNews-vectors-negative300.bin from Google Drive
+# Reference: https://code.google.com/archive/p/word2vec/
+
+# wget --no-check-certificate "https://drive.google.com/uc?export=download&id=0B7XkCwpI5KDYNlNUTTlSS21pQmM" -O GoogleNews-vectors-negative300.bin.gz
+# gunzip GoogleNews-vectors-negative300.bin.gz
