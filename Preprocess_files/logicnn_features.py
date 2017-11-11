b652805f14c175434909638f812c3dba71428847
106SEgroup1 <106segroup1@ntu.edu.tw>
2017-11-11 23:44:24 +0800

    update coding type in logicnn_feature.py

--- Preprocess_files/logicnn_features.py
+++ Preprocess_files/logicnn_features.py
@@ -17,30 +17,31 @@ from pycorenlp import StanfordCoreNLP
 nlp = StanfordCoreNLP('http://localhost:9000')
 warnings.filterwarnings("ignore")
 
+
 def ibm():
     data_file = sys.argv[1]
     USERNAME = sys.argv[2]
     PASSWORD = sys.argv[3]
     tone_analyzer = ToneAnalyzerV3(username=USERNAME, password=PASSWORD, version='2017-10-23')
-    print ("loading data...")
+    print("loading data...")
     x_text, y = load_npy_data(data_file)
-    print ("data loaded!")
+    print("data loaded!")
     rule1_fea = extract_rule_ibm(x_text, tone_analyzer)
     fea_file = splitext(data_file)[0] + '.feaIBM.npy'
     np.save(fea_file, rule1_fea)
-    print ("feature dumped!")
+    print("feature dumped!")
 
 
 def main():
     data_file = sys.argv[1]
-    print ("loading data...")
+    print("loading data...")
     x_text, y = load_npy_data(data_file)
     # revs, W, W2, word_idx_map, vocab = x[0], x[1], x[2], x[3], x[4]
-    print ("data loaded!")
+    print("data loaded!")
     rule1_fea = extract_rule1(x_text)
     fea_file = splitext(data_file)[0] + '.fea.npy'
     np.save(fea_file, rule1_fea)
-    print ("feature dumped!")
+    print("feature dumped!")
     # nlp_server.stop()
 
 
@@ -49,6 +50,7 @@ def text_after_first(text, part):
         return ''.join(text.split(part)[1:])
     else:
         return ''
+
 
 """
 def extract_but(revs):
@@ -99,7 +101,7 @@ def extract_rule1(revs):
                             'outputFormat': 'json'})
             senti_res = (int(res["sentences"][0]["sentimentValue"]) - 1) / 2.0 if res["sentences"] != [] else 0.5
             senti_ori = (int(ori["sentences"][0]["sentimentValue"]) - 1) / 2.0 if ori["sentences"] != [] else 0.5
-            rule1_senti.append((senti_ori-senti_res)*0.5+0.5)
+            rule1_senti.append((senti_ori - senti_res) * 0.5 + 0.5)
             print('Found \'love\'. Finish annotation.')
         else:
             rule1_ind.append(0)
@@ -151,6 +153,7 @@ def load_npy_data(data_file):
         y[i][v] = 1
     return X, y
 
+
 def extract_rule_ibm(revs, tone_analyzer):
     rule1_fea = []
     rule1_ind = []
@@ -168,7 +171,7 @@ def extract_rule_ibm(revs, tone_analyzer):
 
             res = tone_analyzer.tone(text=fea, sentences=True, content_type='text/plain')
             ori = tone_analyzer.tone(text=text, sentences=True, content_type='text/plain')
-            defaultvalue = [('joy',0.0), ('fear',0.0), ('sadness',0.0), ('anger',0.0), ('analytical',0.0), ('confident',0.0), ('tentative',0.0)]
+            defaultvalue = [('joy', 0.0), ('fear', 0.0), ('sadness', 0.0), ('anger', 0.0), ('analytical', 0.0), ('confident', 0.0), ('tentative', await0.0)]
             res_score, ori_score = dict(defaultvalue), dict(defaultvalue)
             for t in ori['document_tone']['tones']:
                 if t['tone_id'] in ori_score.keys():
@@ -178,7 +181,7 @@ def extract_rule_ibm(revs, tone_analyzer):
                     res_score[t['tone_id']] = t['score']
             ans = ori_score.copy()
             for item in ori_score.keys():
-                ans[item]=(ori_score[item]-res_score[item])*0.5+0.5
+                ans[item] = (ori_score[item] - res_score[item]) * 0.5 + 0.5
             rule1_senti.append(ans)
             print('Found \'love\'. Finish annotation.')
         else:
@@ -194,5 +197,5 @@ def extract_rule_ibm(revs, tone_analyzer):
 
 
 if __name__ == "__main__":
-    #main()
+    # main()
     ibm()
